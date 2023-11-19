a63bfa654341e61674209673aa1142121887c5b3
m-aouzal <114601239+m-aouzal@users.noreply.github.com>
2023-11-20 00:56:49 +0100

    add autocomplete for the current selection

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,74 @@
-import { Component } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatCardModule } from '@angular/material/card';
 
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AsyncPipe } from '@angular/common';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatCardModule, FlexLayoutModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    FlexLayoutModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    AsyncPipe,
+  ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  @ViewChild('input') input: ElementRef<HTMLInputElement>;
+  myControl = new FormControl('');
+  options: string[] = [
+    'Carpenter',
+    'Electrician',
+    'Painter',
+    'Mechanic',
+    'Chef',
+    'Developer',
+    'Designer',
+    'Writer',
+    'Doctor',
+    'Teacher',
+    'Plumber',
+    'Engineer',
+    'Artist',
+    'Photographer',
+    'Accountant',
+    'Lawyer',
+    'Nurse',
+    'Architect',
+    'Scientist',
+    'Librarian',
+    'Pilot',
+    'Farmer',
+    'Police Officer',
+    'Firefighter',
+    'Salesperson',
+    'Barista',
+    'Musician',
+    'Athlete',
+  ];
+
+  filteredOptions: string[];
+
+  constructor() {
+    this.filteredOptions = this.options.slice();
+  }
+
+  filter(): void {
+    const filterValue = this.input.nativeElement.value.toLowerCase();
+    this.filteredOptions = this.options.filter((o) =>
+      o.toLowerCase().includes(filterValue)
+    );
+  }
+}
