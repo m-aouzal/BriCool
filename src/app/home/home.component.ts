e31001d449ab32c4eb6177e5389488cbfc74227b
m-aouzal <114601239+m-aouzal@users.noreply.github.com>
2023-11-20 16:11:50 +0100

    added dummy taskers

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AsyncPipe } from '@angular/common';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
@@ -10,6 +10,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { Project } from './project';
+import { Tasker } from './tasker';
 
 @Component({
   selector: 'app-home',
@@ -70,6 +71,61 @@ export class HomeComponent {
     { title: 'Project 4', src: 'path/to/image4.jpg', min: 200, max: 300 },
     { title: 'Project 5', src: 'path/to/image5.jpg', min: 80, max: 150 },
     { title: 'Project 6', src: 'path/to/image6.jpg', min: 170, max: 220 },
+  ];
+  dummyTaskers: Tasker[] = [
+    {
+      name: 'John Doe',
+      slogan: 'Your Trusted Handyman',
+      description:
+        'Experienced in various home repair tasks. Customer satisfaction is my top priority!',
+      rating: 95,
+      img: 'john-doe.jpg', // Placeholder image filename
+      skills: [
+        { description: 'Plumbing', pricePerHour: 30 },
+        { description: 'Electrical Repair', pricePerHour: 35 },
+        { description: 'Carpentry', pricePerHour: 25 },
+      ],
+      completedTaskNumber: 120,
+    },
+    {
+      name: 'Jane Smith',
+      slogan: 'Expert Painter',
+      description:
+        'Transforming spaces with a brush and creativity. Bringing colors to life!',
+      rating: 88,
+      img: 'jane-smith.jpg', // Placeholder image filename
+      skills: [
+        { description: 'Interior Painting', pricePerHour: 40 },
+        { description: 'Exterior Painting', pricePerHour: 45 },
+      ],
+      completedTaskNumber: 80,
+    },
+    {
+      name: 'Mike Johnson',
+      slogan: 'Your Friendly IT Guy',
+      description:
+        'Solving tech issues with a smile. From troubleshooting to setting up networks!',
+      rating: 78,
+      img: 'mike-johnson.jpg', // Placeholder image filename
+      skills: [
+        { description: 'Computer Repair', pricePerHour: 40 },
+        { description: 'Network Setup', pricePerHour: 35 },
+      ],
+      completedTaskNumber: 60,
+    },
+    {
+      name: 'Emily Davis',
+      slogan: 'Master Gardener',
+      description:
+        'Passionate about plants and landscapes. Turning gardens into works of art!',
+      rating: 92,
+      img: 'emily-davis.jpg', // Placeholder image filename
+      skills: [
+        { description: 'Landscaping', pricePerHour: 30 },
+        { description: 'Plant Care', pricePerHour: 25 },
+      ],
+      completedTaskNumber: 100,
+    },
   ];
 
   filteredOptions: string[];
---
+++ src/app/home/tasker.ts
@@ -0,0 +1,14 @@
+import { Skill } from './skill';
+
+export interface Tasker {
+  name: string;
+  slogan: string;
+  description: string;
+  rating: number;
+  img: string;
+  skills: Skill[];
+  completedTaskNumber: number;
+}
+
+
+
