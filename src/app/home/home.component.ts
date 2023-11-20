77c615ca81713f6280fe23409f3706ae66383b93
m-aouzal <114601239+m-aouzal@users.noreply.github.com>
2023-11-20 15:47:34 +0100

    added everyday life

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,6 +9,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { Project } from './project';
 
 @Component({
   selector: 'app-home',
@@ -61,6 +62,14 @@ export class HomeComponent {
     'Barista',
     'Musician',
     'Athlete',
+  ];
+  projects: Project[] = [
+    { title: 'Project 1', src: 'path/to/image1.jpg', min: 100, max: 200 },
+    { title: 'Project 2', src: 'path/to/image2.jpg', min: 150, max: 250 },
+    { title: 'Project 3', src: 'path/to/image3.jpg', min: 120, max: 180 },
+    { title: 'Project 4', src: 'path/to/image4.jpg', min: 200, max: 300 },
+    { title: 'Project 5', src: 'path/to/image5.jpg', min: 80, max: 150 },
+    { title: 'Project 6', src: 'path/to/image6.jpg', min: 170, max: 220 },
   ];
 
   filteredOptions: string[];
---
+++ src/app/home/project.ts
@@ -0,0 +1,6 @@
+export interface Project {  
+    title:string;
+    src:string;
+    min:number;
+    max:number;
+}
