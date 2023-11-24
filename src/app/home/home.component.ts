64a0dfe78f44d48442af839e142e815322d85d40
m-aouzal <114601239+m-aouzal@users.noreply.github.com>
2023-11-24 11:46:02 +0100

    all done, add also home as input

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -64,35 +64,7 @@ export class HomeComponent {
   ];
 
  
-  projects: Project[] = [
-    {
-      title: 'Plumbing Upgrade',
-      src: 'aouzal.jpeg',
-      min: 100,
-      max: 200,
-    },
-    {
-      title: 'Electrical Renovation',
-      src: 'path/to/image2.jpg',
-      min: 150,
-      max: 250,
-    },
-    { title: 'Deep Cleaning', src: 'path/to/image3.jpg', min: 120, max: 180 },
-    {
-      title: 'New Construction',
-      src: 'path/to/image4.jpg',
-      min: 200,
-      max: 300,
-    },
-    {
-      title: 'Apartment Cleaning',
-      src: 'path/to/image5.jpg',
-      min: 80,
-      max: 150,
-    },
-    { title: 'Office Upgrade', src: 'path/to/image6.jpg', min: 170, max: 220 },
-  ];
-
+ 
   taskers: Tasker[] = [
     {
       name: 'Ali Mohamed',
--- src/app/home/projects-card/projects-card.component.ts
+++ src/app/home/projects-card/projects-card.component.ts
@@ -5,11 +5,37 @@ import { MatCardModule } from '@angular/material/card';
 @Component({
   selector: 'app-projects-card',
   standalone: true,
-  imports: [CommonModule,
-    MatCardModule],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './projects-card.component.html',
-  styleUrls: ['./projects-card.component.css']
+  styleUrls: ['./projects-card.component.css'],
 })
 export class ProjectsCardComponent {
-
+  projects: Project[] = [
+    {
+      title: 'Plumbing Upgrade',
+      src: 'aouzal.jpeg',
+      min: 100,
+      max: 200,
+    },
+    {
+      title: 'Electrical Renovation',
+      src: 'path/to/image2.jpg',
+      min: 150,
+      max: 250,
+    },
+    { title: 'Deep Cleaning', src: 'path/to/image3.jpg', min: 120, max: 180 },
+    {
+      title: 'New Construction',
+      src: 'path/to/image4.jpg',
+      min: 200,
+      max: 300,
+    },
+    {
+      title: 'Apartment Cleaning',
+      src: 'path/to/image5.jpg',
+      min: 80,
+      max: 150,
+    },
+    { title: 'Office Upgrade', src: 'path/to/image6.jpg', min: 170, max: 220 },
+  ];
 }
