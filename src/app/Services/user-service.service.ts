993a13928ba8aa43ccf0f0076da6e7449590b4f9
m-aouzal <114601239+m-aouzal@users.noreply.github.com>
2023-12-14 08:50:29 +0100

    just added the service

--- src/app/Services/user-service.service.ts
+++ src/app/Services/user-service.service.ts
@@ -1,20 +1,19 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {Seller} from "../Interfaces/seller";
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Seller } from '../Interfaces/seller';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserServiceService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  baseurl = "";
+  baseurl = '';
   getSeller(): Observable<any> {
     return this.http.get<any>(`${this.baseurl}`);
   }
-  public postSeller(employee: Seller): Observable<Seller>{
-    return this.http.post<Seller>(`${this.baseurl}`,employee)
+  public postSeller(employee: Seller): Observable<Seller> {
+    return this.http.post<Seller>(`${this.baseurl}`, employee);
   }
 }
