566aad4c266d2d14bd91e9188233c480f46f3bf0
m-aouzal <114601239+m-aouzal@users.noreply.github.com>
2023-12-18 17:38:43 +0100

    Add HttpClientModule to providers

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app-routing';
 import { provideRouter } from '@angular/router';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes), provideAnimations(), BrowserModule]
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    provideHttpClient(),
+    BrowserModule,
+    HttpClientModule,
+  ],
 }).catch((err) => console.error(err));
