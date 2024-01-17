c0d322614d7f5ce0fe1364e2b78e54bbaef74346
Siaouzal <aouzal1999@gmail.com>
2024-01-17 15:20:13 +0100

    Update Angular project configuration and add AuthService

---
+++ src/environments/environment.ts
@@ -0,0 +1,27 @@
+
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyBp9w4oil77XT3FYITOpZrQ5Iozpq4y3_g',
+    authDomain: 'bricool-ginf.firebaseapp.com',
+    databaseURL: 'https://bricool-ginf-default-rtdb.firebaseio.com',
+    projectId: 'bricool-ginf',
+    storageBucket: 'bricool-ginf.appspot.com',
+    messagingSenderId: '160115731845',
+    appId: '1:160115731845:web:171092878c4d83baf1e8ec',
+    measurementId: 'G-75LJXWH7CW',
+  },
+  production: false,
+  apiUrl: 'https://us-central1-get-stream-io-decoded.cloudfunctions.net',
+  stream: {
+    key: 'wxgsgqrws5v8'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
